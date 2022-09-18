47e2b2f6c0bd743b56b8ecfc9c962ae809c0ed73
akashdev99 <akashnandan9@gmail.com>
2022-09-18 12:55:08 +0530

    test change

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LeetTracker',
-      theme: ThemeData(
+      theme: ThemeData(   
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the

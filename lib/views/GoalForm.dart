eeb028bda440bd2b8b450928bdcf3f0b812ec5af
akashnandan99 <akashnandan99@gmail.com>
2022-10-27 22:33:47 +0530

    form boiler plate style done (finctionality not done)

--- lib/views/GoalForm.dart
+++ lib/views/GoalForm.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:date_field/date_field.dart';
+import 'package:weekday_selector/weekday_selector.dart';
 
 class GoalForm extends StatefulWidget {
   const GoalForm({super.key});
@@ -10,6 +12,12 @@ class GoalForm extends StatefulWidget {
 }
 
 class _GoalFormState extends State<GoalForm> {
+  final _formKey = GlobalKey<FormState>();
+  int? lastTapped;
+  printIntAsDay(int day) {
+    print('Received integer: $day. Corresponds to day: ${day}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +29,82 @@ class _GoalFormState extends State<GoalForm> {
             backgroundColor: Theme.of(context).backgroundColor,
             foregroundColor: Colors.black,
             elevation: 0),
-        body: Center(child: Text("GOals")));
+        body: Form(
+            key: _formKey,
+            child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                        // The validator receives the text that the user has entered.
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "Task Name")),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    DateTimeFormField(
+                      decoration: const InputDecoration(
+                        hintStyle: TextStyle(color: Colors.black45),
+                        errorStyle: TextStyle(color: Colors.redAccent),
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.event_note),
+                        labelText: 'Only time',
+                      ),
+                      mode: DateTimeFieldPickerMode.date,
+                      autovalidateMode: AutovalidateMode.always,
+                      validator: (e) => (e?.day ?? 0) == 1
+                          ? 'Please not the first day'
+                          : null,
+                      onDateSelected: (DateTime value) {
+                        print(value);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    WeekdaySelector(
+                      // We display the last tapped value in the example app
+                      onChanged: (v) {
+                        printIntAsDay(v);
+                        setState(() => lastTapped = v);
+                      },
+                      values: const [
+                        true, // Sunday
+                        false, // Monday
+                        false, // Tuesday
+                        false, // Wednesday
+                        false, // Thursday
+                        false, // Friday
+                        true, // Saturday
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Validate returns true if the form is valid, or false otherwise.
+                          if (_formKey.currentState!.validate()) {
+                            // If the form is valid, display a snackbar. In the real world,
+                            // you'd often call a server or save the information in a database.
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Processing Data')),
+                            );
+                          }
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ),
+                  ],
+                ))));
   }
 }

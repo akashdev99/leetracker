cf9b8ada7040f1e88a2a2f8c224d29b61b1a9950
akashnandan99 <akashnandan99@gmail.com>
2022-11-01 21:48:07 +0530

    Notifcation setup

---
+++ lib/Notification/localNotification.dart
@@ -0,0 +1,4 @@
+// https://www.kodeco.com/34019063-creating-local-notifications-in-flutter
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tzData;
